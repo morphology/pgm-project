d166475bb0aa4281a76a01a3ea7036e6183f47f7
Victor Chahuneau <vchahune@cs.cmu.edu>
2013-04-20 15:17:20 -0400

    Add decoding and log-likelihood

--- umorph2/model.py
+++ umorph2/model.py
@@ -1,3 +1,4 @@
+import math
 import random
 from vpyp.pyp import CRP
 from vpyp.prob import mult_sample
@@ -7,16 +8,33 @@ def segmentations(word):
         yield word[:k], word[k:]
 
 class Multinomial:
-    """A multinomial distribution sampled from a prior"""
+    """Non-collapsed multinomial distribution sampled from a prior"""
     def __init__(self, K, prior):
         self.prior = prior
-        self.theta = prior.sample([0]*K)
+        self.K = K
+        self.counts = [0]*K
+        self.N = 0
+
+    def increment(self, k):
+        assert (0 <= k < self.K)
+        self.counts[k] += 1
+        self.N += 1
+
+    def decrement(self, k):
+        assert (0 <= k < self.K)
+        self.counts[k] -= 1
+        self.N -= 1
 
     def prob(self, k):
         return self.theta[k]
 
-    def resample(self, counts):
-        self.theta = self.prior.sample(counts)
+    def resample(self):
+        self.theta = self.prior.sample(self.counts)
+
+    def log_likelihood(self):
+        return (math.lgamma(self.N + 1) + sum(c * math.log(self.theta[k]) - math.lgamma(c + 1)
+            for k, c in enumerate(self.counts) if c > 0)
+            + self.prior.log_likelihood(self.theta))
 
 class Dirichlet:
     """A Dirichlet distribution for sampling multinomials"""
@@ -29,18 +47,36 @@ class Dirichlet:
         norm = sum(sample)
         return [v/norm for v in sample]
 
+    def log_likelihood(self, theta):
+        K = len(theta)
+        return (math.lgamma(K * self.alpha) - K * math.lgamma(self.alpha)
+                + sum((self.alpha - 1) * math.log(t) for t in theta))
+
+
 class MultProduct:
     """H(p, s) = theta_p(p) * theta_s(s)"""
     def __init__(self, n_prefixes, alpha_p, n_suffixes, alpha_s):
         self.theta_p = Multinomial(n_prefixes, Dirichlet(alpha_p))
         self.theta_s = Multinomial(n_suffixes, Dirichlet(alpha_s))
 
+    def increment(self, p, s):
+        self.theta_p.increment(p)
+        self.theta_s.increment(s)
+
+    def decrement(self, p, s):
+        self.theta_p.decrement(p)
+        self.theta_s.decrement(s)
+
     def prob(self, p, s):
         return self.theta_p.prob(p) * self.theta_s.prob(s)
 
-    def resample(self, counts_p, counts_s):
-        self.theta_p.resample(counts_p)
-        self.theta_s.resample(counts_s)
+    def resample(self):
+        self.theta_p.resample()
+        self.theta_s.resample()
+
+    def log_likelihood(self):
+        #return self.theta_p.log_likelihood() + self.theta_s.log_likelihood()
+        return 0 # numerical precision errors make LL computation impossible
 
 class SegmentationModel(CRP):
     """SegmentationModel ~ DP(alpha, H)"""
@@ -62,9 +98,9 @@ class SegmentationModel(CRP):
             if n < c: return i
             n -= c
 
-    def seating_probs(self, k, initialize=False):
-        """Joint probabilities of all possible (segmentation, table assignments)"""
-        for prefix, suffix in segmentations(self.word_vocabulary[k]):
+    def seating_probs(self, w, initialize=False):
+        """Joint probabilities of all possible (segmentation, table assignments) of word #w"""
+        for prefix, suffix in segmentations(self.word_vocabulary[w]):
             p = self.prefix_vocabulary[prefix]
             s = self.suffix_vocabulary[suffix]
             yield (p, s, -1), (1 if initialize else self.alpha * self.base.prob(p, s))
@@ -72,13 +108,31 @@ class SegmentationModel(CRP):
             for seat, count in enumerate(self.tables[(p, s)]):
                 yield (p, s, seat), (1 if initialize else count)
 
-    def increment(self, k, initialize=False):
-        """Sample a segmentation and a table assignment for word k"""
-        (p, s, seat) = mult_sample(self.seating_probs(k, initialize))
-        self._seat_to((p, s), seat)
+    def increment(self, w, initialize=False):
+        """Sample a segmentation and a table assignment for word #w"""
+        (p, s, seat) = mult_sample(self.seating_probs(w, initialize))
+        if self._seat_to((p, s), seat):
+            self.base.increment(p, s)
         return (p, s)
 
     def decrement(self, p, s):
         """Decrement the count for a (p, s) segmentation"""
         seat = self._random_table((p, s))
-        self._unseat_from((p, s), seat)
+        if self._unseat_from((p, s), seat):
+            self.base.decrement(p, s)
+
+    def segmentation_probs(self, w):
+        """Marginal probabilities of all segmentations of word #w"""
+        for prefix, suffix in segmentations(self.word_vocabulary[w]):
+            p = self.prefix_vocabulary[prefix]
+            s = self.suffix_vocabulary[suffix]
+            prob = self.alpha * self.base.prob(p, s) + self.ncustomers.get((p, s), 0)
+            yield (p, s), prob
+
+    def decode(self, w):
+        """Compute the most likely segmentation (p, s) of word #w"""
+        return max(self.segmentation_probs(w), key=lambda t:t[1])[0]
+
+    def log_likelihood(self):
+        return (math.lgamma(self.alpha) - math.lgamma(self.alpha + self.total_customers)
+                + self.ntables * math.log(self.alpha) + self.base.log_likelihood())
--- umorph2/train.py
+++ umorph2/train.py
@@ -1,27 +1,47 @@
 import sys
 import logging
+import math
+import heapq
+from itertools import izip
 from vpyp.corpus import Vocabulary
 from model import segmentations, SegmentationModel
 
+def show_top(model):
+    top_prefixes = heapq.nlargest(10, izip(model.base.theta_p.counts, model.prefix_vocabulary))
+    n_prefixes = sum(1 for c in model.base.theta_p.counts if c > 0)
+    logging.info('Top prefixes (10/%d): %s', n_prefixes, ' '.join(prefix for _, prefix in top_prefixes))
+    top_suffixes = heapq.nlargest(10, izip(model.base.theta_s.counts, model.suffix_vocabulary))
+    n_suffixes = sum(1 for c in model.base.theta_s.counts if c > 0)
+    logging.info('Top suffixes (10/%d): %s', n_suffixes, ' '.join(suffix for _, suffix in top_suffixes))
+
 def run_sampler(model, n_iter, words):
     logging.info('Initializing')
-    analyses = [model.increment(word, initialize=True) for word in words]
+    model.base.resample() # Initialize H
+    analyses = [model.increment(word, initialize=True) for word in words] # Initialize G, (p, s)
     for it in xrange(n_iter):
-        logging.info('Iteration %d/%d', it+1, n_iter)
+        if it % 10 == 0:
+            logging.info('Iteration %d/%d', it+1, n_iter)
+            LL = model.log_likelihood()
+            ppl = math.exp(-LL/len(words))
+            logging.info('LL=%.0f ppl=%.0f', LL, ppl)
+            show_top(model)
         # 1. resample seat assignments and table labels given H
         for w in xrange(len(words)):
             model.decrement(*analyses[w])
             analyses[w] = model.increment(words[w])
-        # 2. resample H given table tables
-        counts_p = [0]*len(model.prefix_vocabulary)
-        counts_s = [0]*len(model.suffix_vocabulary)
-        for (p, s), tables in model.tables.iteritems():
-            counts_p[p] += len(tables)
-            counts_s[s] += len(tables)
-        model.base.resample(counts_p, counts_s)
+        # 2. resample H given table labels
+        model.base.resample()
+
+
+def show_analyses(model):
+    for w, word in enumerate(model.word_vocabulary):
+        p, s = model.decode(w)
+        prefix = model.prefix_vocabulary[p]
+        suffix = model.suffix_vocabulary[s]
+        print(u'{}\t_\t{}\t{}'.format(word, prefix, suffix).encode('utf8'))
 
 def main():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
 
     word_vocabulary = Vocabulary(start_stop=False)
     corpus = [word_vocabulary[line.decode('utf8').strip()] for line in sys.stdin]
@@ -41,6 +61,8 @@ def main():
             prefix_vocabulary, suffix_vocabulary)
 
     run_sampler(model, 1000, corpus)
+
+    show_analyses(model)
 
 if __name__ == '__main__':
     main()
